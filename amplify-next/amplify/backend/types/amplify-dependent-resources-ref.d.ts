6fa07fec930fc491b576213ed79f646879a34fed
iukea <jordan.chaput@outlook.com>
2022-04-11 02:31:38 -0700

    test

---
+++ amplify-next/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "NextBlog": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "projectimages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
