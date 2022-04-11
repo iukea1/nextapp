bc6b3a30bf899873dbad2be9d360baed3318bfa0
iukea <jordan.chaput@outlook.com>
2022-04-11 15:36:58 -0700

    yes

--- amplify-next/myapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-next/myapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,19 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
         "myapp": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "myapp900be3a5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
         }
     }
 }
