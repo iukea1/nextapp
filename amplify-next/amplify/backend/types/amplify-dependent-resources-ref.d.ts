0fafe308b00983cb6c4a890be82af957665001d8
iukea <jordan.chaput@outlook.com>
2022-04-29 21:32:15 -0700

    yes'

--- amplify-next/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "amplifynext": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
